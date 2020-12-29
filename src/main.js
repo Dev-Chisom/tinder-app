ac2becfc3d4d97be108654d8813c9fcc971c72c5
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2020-12-29 21:20:20 +0100

    my first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAaH2b0uPRNuHOcJJeZRtPPUUShRB2cMic",
+  authDomain: "tinder-app-1233e.firebaseapp.com",
+  projectId: "tinder-app-1233e",
+  storageBucket: "tinder-app-1233e.appspot.com",
+  messagingSenderId: "626191238662",
+  appId: "1:626191238662:web:a4bf1ec09bd29aeeb1e587"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store/Index'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: guard
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "sign-up" */ '../views/SignUp.vue')
+  },
+  {
+    path: '/sign-in',
+    name: 'Signin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "sign-in" */ '../views/Signin.vue')
   },
   {
     path: '/about',
@@ -16,10 +34,16 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "sign-in" */ '../views/About.vue')
+  },
 ]
-
+function guard(to, from, next){
+  if(store.state.user.id){
+    next();
+  }else{
+    next('/')
+  }
+}
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
